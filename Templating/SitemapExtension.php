c07f81e1231c80e0e8fef59d230230b5af303211
ornicar <thibault.duplessis@gmail.com>
2011-02-09 17:01:42 -0800

    Remove request dependency from twig extension. Add unit tests for it.

--- Templating/SitemapExtension.php
+++ Templating/SitemapExtension.php
@@ -2,25 +2,29 @@
 
 namespace Bundle\Avalanche\SitemapBundle\Templating;
 
-use Symfony\Component\HttpFoundation\Request;
+use InvalidArgumentException;
 
 class SitemapExtension extends \Twig_Extension
 {
-    private $request;
+    private $baseUrl;
+    private $scheme;
 
-    public function __construct(Request $request = null)
+    public function __construct($baseUrl)
     {
-        $this->request = $request;
+        $this->baseUrl = trim($baseUrl, '/');
+        $this->scheme = preg_replace('#^(\w+)://.+$#', '$1', $baseUrl);
+
+        if(!in_array($this->scheme, $this->getKnownSchemes())) {
+            throw new InvalidArgumentException(sprintf('Base url "%s" does not have a valid scheme', $baseUrl));
+        }
     }
 
     public function getFilters()
     {
-        if (isset($this->request)) {
-            return array(
-                'sm_absolutize' => new \Twig_Filter_Method($this, 'getAbsoluteUrl'),
-                'sm_format_date' => new \Twig_Filter_Method($this, 'getFormattedDate'),
-            );
-        }
+        return array(
+            'sm_absolutize' => new \Twig_Filter_Method($this, 'getAbsoluteUrl'),
+            'sm_format_date' => new \Twig_Filter_Method($this, 'getFormattedDate'),
+        );
 
         return parent::getFilters();
     }
@@ -31,9 +35,9 @@ class SitemapExtension extends \Twig_Extension
             return $path;
         }
         if ('//' === substr($path, 0, 2)) {
-            return $this->request->getScheme().':'.$path;
+            return $this->scheme.':'.$path;
         }
-        return $this->request->getUriForPath($path);
+        return $this->baseUrl.$path;
     }
 
     public function getFormattedDate($date)
@@ -44,6 +48,11 @@ class SitemapExtension extends \Twig_Extension
         if ($date instanceof \DateTime) {
             return $date->format('Y-m-d');
         }
+    }
+
+    private function getKnownSchemes()
+    {
+        return array('http', 'https');
     }
 
     public function getName()
---
+++ Tests/Templating/SitemapExtensionTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Bundle\Avalanche\SitemapBundle\Templating;
+
+use PHPUnit_Framework_TestCase;
+
+class SitemapExtensionText extends PHPUnit_Framework_TestCase
+{
+    protected $baseUrl = 'http://sitemaps.org/';
+
+    protected function getExtension()
+    {
+        return new SitemapExtension($this->baseUrl);
+    }
+
+    /**
+     * @expectedException InvalidArgumentException
+     */
+    public function testInstanciateExtensionWithoutProperBaseUrlTrowsAnException()
+    {
+        new SitemapExtension('/invalid/url');
+    }
+
+    public function testGetAbsoluteUrlDoNotModifyAbsoluteUrl()
+    {
+        $url = 'http://site.org/url';
+        $this->assertEquals($url, $this->getExtension()->getAbsoluteUrl($url));
+    }
+
+    public function testGetAbsoluteUrlPrependsBaseUrlToRelativePath()
+    {
+        $path = '/some/path';
+        $expected = 'http://sitemaps.org/some/path';
+        $this->assertEquals($expected, $this->getExtension()->getAbsoluteUrl($path));
+    }
+
+    public function testGetAbsoluteUrlPrependsSchemeToDoubleSlashPath()
+    {
+        $path = '//site.org/some/path';
+        $expected = 'http://site.org/some/path';
+        $this->assertEquals($expected, $this->getExtension()->getAbsoluteUrl($path));
+    }
+}
